5097a904023be948e0c0a136c3170c7022d56534
mysitee <60393978+mystiee@users.noreply.github.com>
2026-05-09 09:31:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -558,11 +558,7 @@ function render() {
     mapa.addEventListener("pointercancel", stop);
     mapa.addEventListener("pointerleave", stop);
 
-
     currentFloor = "1";
-
-    currentFloor = "3";
-
 
     updateView(currentFloor, currentView);
     updateMarkers(currentFloor);
